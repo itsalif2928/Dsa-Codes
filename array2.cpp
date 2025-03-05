3034f070caf859f433dbff31734709e3545cdcdb
Md Asif Rahman Alif <mdalifr2928@gmail.com>
2025-03-05 21:11:43 +0600

    Add files via upload

---
+++ array2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int marks[5] = {99, 95, 54, 47, 71};
+    int size = 5;
+    for(int i =0; i<size; i++){
+        cout<< marks[i] <<endl;
+    }
+
+
+
+return 0;
+
+}
---
+++ name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string name = "alif";
+    for(int i = 0; i < 100; i++){
+        cout<<"My name is : "<<name<<endl;
+
+    }
+    return 0;
+}
